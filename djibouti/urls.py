d113cb180b4cd88ac95a2cfd155e7f8c6c6269ea
Matthew Daly <matthewbdaly@gmail.com>
2013-05-27 19:28:04 +0100

    Added favicon

--- djibouti/urls.py
+++ djibouti/urls.py
@@ -16,6 +16,9 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    # Favicon
+    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
+
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
