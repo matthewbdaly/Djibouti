6d479a486178704f96bc190d01a2b2b03b959557
Matthew Daly <matthewbdaly@gmail.com>
2013-05-27 14:39:27 +0100

    Can now view individual posts

--- djibouti/urls.py
+++ djibouti/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from blog.models import Post
 
 # Uncomment the next two lines to enable the admin:
@@ -21,6 +21,11 @@ urlpatterns = patterns('',
     url(r'^(?P<page>\d+)?/?$', ListView.as_view(
         model=Post,
         paginate_by=5,
+        )),
+
+    # Individual vacancies
+    url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/?$', DetailView.as_view(
+        model=Post,
         )),
 
     # Flat pages
