9630035f370e7106f7656d6eaf8f272f936fb4e2
Matthew Daly <matthewbdaly@gmail.com>
2013-05-27 15:14:10 +0100

    Added categories

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=40, unique=True)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/categories/%s/" % self.slug
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     text = models.TextField()
     slug = models.SlugField(max_length=40, unique=True)
     author = models.ForeignKey(User)
+    categories = models.ManyToManyField(Category, blank=True, null=True, through='CategoryToPost')
 
     class Meta:
         ordering = ["-pub_date"]
@@ -17,3 +34,8 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return "/%s/%s/%s/" % (self.pub_date.year, self.pub_date.month, self.slug)
+
+
+class CategoryToPost(models.Model):
+    post = models.ForeignKey(Post)
+    category = models.ForeignKey(Category)
