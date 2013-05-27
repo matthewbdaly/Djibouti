22e76ef12380ba18b2ff748d0e7950d5e284f24e
Matthew Daly <matthewbdaly@gmail.com>
2013-05-27 23:23:43 +0100

    Added Pygments

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from markdown import markdown
 
 
 # Create your models here.
@@ -22,6 +23,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     text = models.TextField()
+    html_text = models.TextField(editable=False, blank=True, null=True)
     slug = models.SlugField(max_length=40, unique=True)
     author = models.ForeignKey(User)
     categories = models.ManyToManyField(Category, blank=True, null=True, through='CategoryToPost')
@@ -29,11 +31,18 @@ class Post(models.Model):
     class Meta:
         ordering = ["-pub_date"]
 
+
     def __unicode__(self):
         return self.title
 
+
     def get_absolute_url(self):
         return "/%s/%s/%s/" % (self.pub_date.year, self.pub_date.month, self.slug)
+
+
+    def save(self):
+        self.html_text = markdown(self.text, ['codehilite'])
+        super(Post, self).save()
 
 
 class CategoryToPost(models.Model):
