be027f1e2dda20051208607152336669d43b33dd
Matthew Daly <matthewbdaly@gmail.com>
2013-05-27 14:10:33 +0100

    Added blog app

--- djibouti/settings.py
+++ djibouti/settings.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'django.contrib.flatpages',
     'south',
+    'blog',
 )
 
 # A sample logging configuration. The only tangible logging
